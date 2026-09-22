57981b11824e8c020939a9abda8a0419dac7d7b8
Pterasyne <morgan.lepidoptera@gmail.com>
2026-09-22 09:16:31 +0200

    Corrected number of files

--- stratBis/genStratBis.py
+++ stratBis/genStratBis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import random
-NB_PARTICIPANTS = 30 #arbitrary as I don't know precisely yet
-LAST_NB = 26 #we had 26 for the first wave
+NB_PARTICIPANTS = 42 #arbitrary as I don't know precisely yet
+LAST_NB = 0 #we had 26 for the first wave
 
 pairs = []
 for a in range(1, 10):
@@ -20,10 +20,10 @@ for i in range(LAST_NB+1, LAST_NB+NB_PARTICIPANTS+1):
             a+b,
             "",
             "",
-            ] + [""] * 10)
+            ] + [""] * 7)
     columns = [
         "Type", "Opérande 1", "signe", "Opérande 2", "Résultat attendu",
         "Réponse enfant", "Match"
-    ] + [str(i) for i in range(10)]    
+    ] + [str(i) for i in range(7)]    
     df=pd.DataFrame(sheet, columns=columns)
     df.to_excel(f"Strat_{i:02d}.xlsx",index=False)
